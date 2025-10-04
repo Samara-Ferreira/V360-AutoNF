f520cf7c85ad7087b0d196989dfe767050c1e34d
Samara <sferreira@ecomp.uefs.br>
2025-10-04 16:14:31 -0300

    ✨ feat: refactor main function to allow user file selection for PDF and image processing

--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@ from pathlib import Path
 from .extraction.pdf_processor import convert_pdf_to_images
 from .extraction.image_processor import extract_text_from_image
 
+from .utils import choose_file
+from PIL import Image
+from .parsing.data_parser import parse_data_fiscal
+
 # Import the samples directory from our config.py file
 from config import SAMPLES_DIR
 
@@ -11,25 +15,32 @@ def main():
     """
     Main function that executes the complete extraction flow.
     """
-    # Define the PDF file we want to process
-    pdf_file = SAMPLES_DIR / "NFSe_ficticia_layout_completo.pdf"
+    # User chooses the file to be processed 
+    file_to_process = choose_file()
 
-    # 1. Convert the PDF to images
-    paginas_imagem = convert_pdf_to_images(pdf_file)
-    print(f"PDF converted. Total pages: {len(paginas_imagem)}")
+    if file_to_process is None:
+        return
 
-    # 2. Extract text from each image
-    print("Starting text extraction from each page...")
-    texto_completo = []
-    for i, pagina in enumerate(paginas_imagem):
-        print(f"Processing page {i + 1}...")
-        texto_da_pagina = extract_text_from_image(pagina)
-        texto_completo.append(texto_da_pagina)
+    if file_to_process.suffix == ".pdf":
+        image_pages = convert_pdf_to_images(file_to_process)
 
-    # 3. Display the final result
-    print("\n--- TEXT EXTRACTED SUCCESSFULLY ---\n")
-    for i, texto in enumerate(texto_completo):
-        print(f"--- Page {i + 1} ---\n{texto}\n")
+    else:
+        image_pages = [Image.open(file_to_process)]
+
+    # Extract text from each image
+    text_complete = []
+    for i, page in enumerate(image_pages):
+        text_of_page = extract_text_from_image(page)
+        text_complete.append(text_of_page)
+
+
+    # Test: Print the raw extracted text
+    text_final = "\n".join(text_complete)
+    json_result = parse_data_fiscal(text_final)
+    print("\n--- EXTRACTED DATA (JSON) ---\n")
+    print(json_result)
+
 
 if __name__ == "__main__":
     main()
+    
