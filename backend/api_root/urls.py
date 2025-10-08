25e60a05ef9302e414e2fafd31b29a7fa4526798
Samara <sferreira@ecomp.uefs.br>
2025-10-07 23:52:27 -0300

    🗑️ chore: remove unused backend API files and configurations

--- backend/api_root/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.urls import include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api_rest.urls'), name='api_rest_urls'),  # Include the api_rest app URLs
-]
